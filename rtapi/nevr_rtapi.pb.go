60b0d9284de1ca08ff20f8dc2cc551dd413076f1
Andrew Bates <a@sprock.io>
2025-08-19 11:13:51 -0500

    Update generated files

--- rtapi/nevr_rtapi.pb.go
+++ rtapi/nevr_rtapi.pb.go
@@ -7,14 +7,13 @@
 package rtapi
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	gameapi "github.com/echotools/nevr-common/v3/gameapi"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
